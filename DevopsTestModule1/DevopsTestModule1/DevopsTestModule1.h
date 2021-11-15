39186081c52dbbc56f50ff18f5e53bc213cbd79d
sinian666 <91591498+sinian666@users.noreply.github.com>
2021-11-15 11:21:15 +0800

    Create DevopsTestModule1.h

---
+++ DevopsTestModule1/DevopsTestModule1/DevopsTestModule1.h
@@ -0,0 +1,17 @@
+//
+//  DevopsTestModule.h
+//  DevopsTestModule
+//
+//  Created by demo on 2020/11/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DevopsTestModule.
+FOUNDATION_EXPORT double DevopsTestModuleVersionNumber;
+
+//! Project version string for DevopsTestModule.
+FOUNDATION_EXPORT const unsigned char DevopsTestModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevopsTestModule/PublicHeader.h>
+
