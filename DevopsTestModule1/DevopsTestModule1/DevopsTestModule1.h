8f00ddee0790ca41f1adda9b34d6fc1e557fba46
sinian666 <91591498+sinian666@users.noreply.github.com>
2021-11-15 11:22:04 +0800

    Update DevopsTestModule1.h

--- DevopsTestModule1/DevopsTestModule1/DevopsTestModule1.h
+++ DevopsTestModule1/DevopsTestModule1/DevopsTestModule1.h
@@ -1,17 +1,17 @@
 //
-//  DevopsTestModule.h
-//  DevopsTestModule
+//  DevopsTestModule1.h
+//  DevopsTestModule1
 //
 //  Created by demo on 2020/11/5.
 //
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for DevopsTestModule.
-FOUNDATION_EXPORT double DevopsTestModuleVersionNumber;
+//! Project version number for DevopsTestModule1.
+FOUNDATION_EXPORT double DevopsTestModule1VersionNumber;
 
 //! Project version string for DevopsTestModule.
-FOUNDATION_EXPORT const unsigned char DevopsTestModuleVersionString[];
+FOUNDATION_EXPORT const unsigned char DevopsTestModule1VersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DevopsTestModule/PublicHeader.h>
 
